cd1212da160955b83c8856fa6475874b5f20dc55
Sydney Pollak <pollak@mesmo.org>
2021-02-26 07:43:26 +0100

    Aki

--- types/lib/Akinator.d.ts
+++ types/lib/Akinator.d.ts
@@ -1,46 +1,48 @@
-export default Akinator;
-declare class Akinator {
-  constructor(region: any, childMode: any);
-  currentStep: number;
-  region: any;
-  uri: any;
-  urlApiWs: any;
-  uriObj: {
+declare module "aki-api" {
+  class Aki {
+    constructor(region: any, childMode: any);
+    currentStep: number;
+    region: any;
+    uri: any;
+    urlApiWs: any;
+    uriObj: {
+      uid: string;
+      frontaddr: string;
+    };
+    noUri: string;
+    noSession: string;
+    progress: number;
+    childMode: {
+      childMod: boolean;
+      softConstraint: string;
+      questionFilter: string;
+    };
+    queston: string;
+    answers: any[];
+    /**
+     * Starts the akinator session and game.
+     */
+    start(): Promise<void>;
     uid: string;
     frontaddr: string;
-  };
-  noUri: string;
-  noSession: string;
-  progress: number;
-  childMode: {
-    childMod: boolean;
-    softConstraint: string;
-    questionFilter: string;
-  };
-  queston: string;
-  answers: any[];
-  /**
-   * Starts the akinator session and game.
-   */
-  start(): Promise<void>;
-  uid: string;
-  frontaddr: string;
-  session: any;
-  signature: any;
-  question: any;
-  challenge_auth: any;
-  /**
-   * Gets the next question for the akinator session.
-   * @param {BigInteger} answerId the answer to the question
-   */
-  step(answerId: BigInteger): Promise<void>;
-  /**
-   * Reverts the game back a previous step.
-   */
-  back(): Promise<void>;
-  /**
-   * The akinator attempts to make a guess and win the game.
-   */
-  win(): Promise<void>;
-  guessCount: any;
+    session: any;
+    signature: any;
+    question: any;
+    challenge_auth: any;
+    /**
+     * Gets the next question for the akinator session.
+     * @param {BigInteger} answerId the answer to the question
+     */
+    step(answerId: BigInteger): Promise<void>;
+    /**
+     * Reverts the game back a previous step.
+     */
+    back(): Promise<void>;
+    /**
+     * The akinator attempts to make a guess and win the game.
+     */
+    win(): Promise<void>;
+    guessCount: any;
+  }
+  export = Aki;
 }
